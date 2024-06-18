4c3c1830bab2df8e4eadecda90ba83d37d4c1425
Chloé Léveillé <chloe.leveille@quebecormedia.com>
2024-06-17 22:14:06 -0400

    scan

--- Sources/Slingshot/Typeclasses/Sequence/Foldable.swift
+++ Sources/Slingshot/Typeclasses/Sequence/Foldable.swift
@@ -25,3 +25,57 @@ public extension Sequence where Self: Monoid & Pure, Element == Self.PureA {
         }
     }
 }
+
+public struct ScanSequence<OriginalSequence, Output>: Sequence where OriginalSequence: Sequence {
+    private let sequence: OriginalSequence
+    private let initialValue: Output
+    private let transform: (Output, OriginalSequence.Element) -> Output
+    
+    init(sequence: OriginalSequence,
+         initialValue: Output,
+         transform: @escaping (Output, OriginalSequence.Element) -> Output) {
+        self.sequence = sequence
+        self.initialValue = initialValue
+        self.transform = transform
+    }
+    
+    public struct Iterator: IteratorProtocol {
+        private var iterator: OriginalSequence.Iterator
+        private var lastResult: Output?
+        private let transform: (Output, OriginalSequence.Element) -> Output
+        
+        init(iterator: OriginalSequence.Iterator,
+             initialValue: Output,
+             transform: @escaping (Output, OriginalSequence.Element) -> Output) {
+            self.iterator = iterator
+            self.lastResult = initialValue
+            self.transform = transform
+        }
+        
+        public mutating func next() -> Output? {
+            guard let lastResult else {
+                return nil
+            }
+            
+            self.lastResult = iterator.next().map { transform(lastResult, $0) }
+            
+            return lastResult
+        }
+    }
+    
+    public func makeIterator() -> Iterator {
+        .init(iterator: sequence.makeIterator(), initialValue: initialValue, transform: transform)
+    }
+}
+
+public extension Sequence {
+    func scan<Output>(_ initialValue: Output,
+                      transform: @escaping (Output, Element) -> Output) -> ScanSequence<Self, Output> {
+        ScanSequence(sequence: self, initialValue: initialValue, transform: transform)
+    }
+    
+    func scan<Output>(transform: @escaping (Output, Element) -> Output) -> ScanSequence<Self, Output>
+    where Output: Zero {
+        ScanSequence(sequence: self, initialValue: .zero, transform: transform)
+    }
+}
